72230d2d788a5d91045883f439ef03c50decd0a2
Mario Orellana <ma_oa08@hotmail.com>
2024-10-25 21:44:45 -0600

    implement dependency injection

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PuntoDeVenta.Persistence;
+using PuntoDeVenta.Persistence.Seeders;
+
 namespace PuntoDeVenta
 {
     internal static class Program
     {
+        public static IServiceProvider serviceProvider { get; private set; }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -11,7 +18,19 @@ namespace PuntoDeVenta
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+
+            // Inject dependencies
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            serviceProvider = serviceCollection.BuildServiceProvider();
+
+            Application.Run(serviceProvider.GetRequiredService<MainForm>());
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            // Register forms here
+            services.AddTransient<MainForm>();
         }
     }
 }
