fe930f4cf2f540569627d34bb096468a0c68b0de
Mario Orellana <ma_oa08@hotmail.com>
2024-10-25 22:24:08 -0600

    implement login logic

--- MainForm.cs
+++ MainForm.cs
@@ -1,10 +1,34 @@
+using PuntoDeVenta.Controls.MainMenu;
+using PuntoDeVenta.Persistence.Seeders;
+
 namespace PuntoDeVenta
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+        private readonly IApplicationSeeder _seeder;
+        private readonly LoginControl _loginControl;
+        private readonly MainMenuControl _mainMenuControl;
+
+        public MainForm(IApplicationSeeder seeder, LoginControl loginControl, MainMenuControl mainMenuControl)
         {
+            _seeder = seeder ?? throw new ArgumentNullException(nameof(seeder));
+            _loginControl = loginControl ?? throw new ArgumentNullException(nameof(loginControl));
+            _mainMenuControl = mainMenuControl ?? throw new ArgumentNullException(nameof(mainMenuControl));
+
             InitializeComponent();
+            _seeder.SeedDatabase();
+
+            // Add login control
+            _loginControl.LoginSuccess += LoginControl_LoginSuccess;
+            _loginControl.Size = this.Size;
+            this.Controls.Add(_loginControl);
+        }
+
+        private void LoginControl_LoginSuccess(object? sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            _mainMenuControl.Size = this.Size;
+            this.Controls.Add(_mainMenuControl);
         }
     }
 }
